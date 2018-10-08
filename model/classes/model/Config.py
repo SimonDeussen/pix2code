f48f19d07ad031288c63023d9635c9ea4c28eca0
SimonDeussen <simon.deussen@gmail.com>
2018-10-08 12:49:08 +0200

    updated cnfig

--- model/classes/model/Config.py
+++ model/classes/model/Config.py
@@ -1,7 +1,7 @@
 __author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
 
-CONTEXT_LENGTH = 48
+CONTEXT_LENGTH = 96
 IMAGE_SIZE = 256
 BATCH_SIZE = 64
-EPOCHS = 100
+EPOCHS = 10
 STEPS_PER_EPOCH = 72000
